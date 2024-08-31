4a916766188cbf882a37cee4a916d6e64b53cba6
meerjan <janushaik9191@gmail.com>
2024-08-31 22:35:36 +0530

    special variables

--- 10-special-variables.sh
+++ 10-special-variables.sh
@@ -4,3 +4,8 @@ echo "all variables passed to the script :: $@"
 echo "total number of variables passed to the script :: $#"
 echo "to find the name of the script :: $0"
 echo "the present working directory :: $PWD"
+echo "the current user home directory :: $HOME"
+echo "the pid of the current executing script :: $$"
+sleep 100 &
+echo  "pid of current background process is :: $!"
+echo "to find the previous command is success or not :: $?"
