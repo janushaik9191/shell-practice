947a166b79e9ca7f46b0be4b8e326b1866c93e20
meerjan <janushaik9191@gmail.com>
2024-08-31 20:21:08 +0530

    conversation bw ramesh and suresh

---
+++ 03-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh :: hi suresh how are you"
+echo "suresh :: hello ramesh iam fine what about you"
+echo "ramesh :: iam learing shell script"
+echo "suresh :: nice to hear you"
