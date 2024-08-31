e26e9db99e47227a23788fd2867d05400d123241
meerjan <janushaik9191@gmail.com>
2024-08-31 21:13:55 +0530

    array of fruit example

--- 07-array.sh
+++ 07-array.sh
@@ -3,4 +3,8 @@
 FRUITS=("apple" "orange" "banana")
 
 echo "the first fruit is :: ${FRUITS[0]}"
-echo "first fruit is :: ${FRUITS[0]}"
+echo "the first fruit is :: ${FRUITS[1]}"
+echo "the first fruit is :: ${FRUITS[2]}"
+
+echo "the first fruit is :: ${FRUITS[@]}"
+
