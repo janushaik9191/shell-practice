065ec0664954e79c0cf0a01d4f03b93f5e73d0ae
meerjan <janushaik9191@gmail.com>
2024-08-31 20:25:21 +0530

    variables case 1

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VARIABLE1=ramesh
+VARIABLE2=suresh
+
+echo "VARIABLE1 :: hi VARIABLE2 how are you"
+echo "VARIABLE2 :: hello VARIABLE1 iam fine what about you"
+echo "VARIABLE1 :: iam learing shell script"
+echo "VARIABLE2 :: nice to hear you"
