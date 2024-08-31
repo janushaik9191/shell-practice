588019d546f0d179670d1b731c755896317ed4d9
meerjan <janushaik9191@gmail.com>
2024-08-31 20:27:00 +0530

    variables case 1

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 VARIABLE1=ramesh
 VARIABLE2=suresh
 
-echo "VARIABLE1 :: hi VARIABLE2 how are you"
-echo "VARIABLE2 :: hello VARIABLE1 iam fine what about you"
-echo "VARIABLE1 :: iam learing shell script"
-echo "VARIABLE2 :: nice to hear you"
+echo "$VARIABLE1 :: hi $VARIABLE2 how are you"
+echo "$VARIABLE2 :: hello $VARIABLE1 iam fine what about you"
+echo "$VARIABLE1 :: iam learing shell script"
+echo "$VARIABLE2 :: nice to hear you"
