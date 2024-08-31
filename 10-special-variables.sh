a106684e80a3afb964a0444350f28653d03fcf09
meerjan <janushaik9191@gmail.com>
2024-08-31 22:29:21 +0530

    special variables

---
+++ 10-special-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "all variables passed to the script :: $@"
+echo "total number of variables passed to the script :: $#"
+echo "to find the name of the script :: $0"
+echo "the present working directory :: $PWD"
