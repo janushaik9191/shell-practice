e3f44c2a2de9fa258a1574fcde860ac27f5a1558
meerjan <janushaik9191@gmail.com>
2024-09-01 20:26:11 +0530

    redirectors of log file

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -7,28 +7,28 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-Y="\e[33m"
+# R="\e[31m"
+# G="\e[32m"
+# N="\e[0m"
+# Y="\e[33m"
 
-CHECK_ROOT(){
+#CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
         echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
-}
+#}
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
-        exit 1
-    else
-        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
-    fi
-}
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+#         exit 1
+#     else
+#         echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+#     fi
+# }
 
 USAGE(){
     echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
@@ -37,7 +37,7 @@ USAGE(){
 
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-CHECK_ROOT
+#CHECK_ROOT
 
 if [ $# -eq 0 ]
 then
