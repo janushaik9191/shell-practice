c8858eceb2648a2fcf6a3d1fcd93a89b86fb11de
meerjan <janushaik9191@gmail.com>
2024-09-01 12:10:49 +0530

    installation with loops

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-USERID=$(id -u)
+    USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "please run the script with root access"
-else
-    exit 1
-fi
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
 
-for package in $@
+for package in $@ # $@ refers to all arguments passed to it
 do
     dnf list installed $package
     if [ $? -ne 0 ]
-    then 
-        echo "$package is not installed going to install it..."
+    then
+        echo "$package is not installed, going to install it.."
         dnf install $package -y
-    else
-        echo "$package is already installed nothing to do.."
+        else
+        echo "$package is already installed..nothing to do"
     fi
 done
