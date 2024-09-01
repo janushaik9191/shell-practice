128d64c8dad18018f9cfe3622c5d0b6e7eb4e72c
meerjan <janushaik9191@gmail.com>
2024-09-01 12:03:06 +0530

    if condition nginx installation

---
+++ 15-loops.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run the script with root access"
+else
+    exit 1
+fi
+
+for package in $@
+do
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then 
+        echo "$package is not installed going to install it"
+        dnf install $package -y
+    else
+        echo "$package is already installed nothing to do.."
+    fi
