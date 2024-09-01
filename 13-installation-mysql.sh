f7afce38c2ae6606565eff5f1ec4b87ac16428a5
meerjan <janushaik9191@gmail.com>
2024-09-01 11:30:12 +0530

    if condition my-sql-installation

--- 13-installation-mysql.sh
+++ 13-installation-mysql.sh
@@ -5,14 +5,18 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "run the script with root access"
-else
-    exit 1
 fi
+
 dnf list installed mysql-server
-  if [ $? -ne 0 ]
-  then
-  echo "mysql server is not installed going to install it"
-  else
-  echo "mysql sertver is already installed" 
-  fi
-  
+    if [ $? -ne 0 ]
+    then
+        echo "mysql server is not installed going to install it"
+        dnf install mysql-server -y
+        if [ $? -ne 0 ]
+        then 
+        echo "mysql-server is is failed to install"
+        else
+        echo "mysql-server installation is success"
+    else
+        echo "mysql server is already installed" 
+    fi
