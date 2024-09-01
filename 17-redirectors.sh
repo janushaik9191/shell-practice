380a6918b5d170c5776a661e1d31b1871cfcadda
meerjan <janushaik9191@gmail.com>
2024-09-01 17:46:51 +0530

    redirectors of log file

--- 17-redirectors.sh
+++ 17-redirectors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOGS_FOLDER="var/log/shell-practice"
-SCRIPT_NAME=$("echo $0 | cut -d "." -f2")
-TIME_STAMP=$(date date +%Y-%m-%d-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f2)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG-FILE=$"$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 mkdir -p $LOGS_FOLDER
