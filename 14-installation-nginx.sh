2c4af1aa183ea2cfdc93db4d333554cea599a15e
meerjan <janushaik9191@gmail.com>
2024-09-01 11:46:30 +0530

    if condition nginx installation

---
+++ 14-installation-nginx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+fi [ $USERID -ne 0 ]
+then
+    echo "plese run the script with root access"
+fi
+    dnf list installed nginx 
+    if [ $? -ne 0 ]
+    then
+        echo "nginx is not installed going to install it..."
+        dnf install nginx -y
+        if [ $? -ne 0 ]
+        then
+            echo "nginx is failed to insatll"
+        else
+            echo "nginx is successfully installed"
+        fi
+    else
+        echo "nginx is already installed nothing to do"
+fi
