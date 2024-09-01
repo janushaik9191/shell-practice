4008de540bf6a104e89ab784675adb6fc6ac67ba
meerjan <janushaik9191@gmail.com>
2024-09-01 11:24:11 +0530

    if condition my-sql-installation

--- 13-installation-mysql.sh
+++ 13-installation-mysql.sh
@@ -8,3 +8,11 @@ then
 else
     exit 1
 fi
+dnf list installed mysql-server
+  if [ $? -ne 0 ]
+  then
+  echo "mysql server is not installed going to install it"
+  else
+  echo "mysql sertver is already installed" 
+  fi
+  
