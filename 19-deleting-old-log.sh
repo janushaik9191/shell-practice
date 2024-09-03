f6ccac5d24d91cdcc0f31212c8433e35c3cd085c
meerjan <janushaik9191@gmail.com>
2024-09-03 20:54:58 +0530

    deleting-old-log files

--- 19-deleting-old-log.sh
+++ 19-deleting-old-log.sh
@@ -4,8 +4,8 @@ SOURCE_DIR=/var/log/shell-script
 
 if [ -d $SOURCE_DIR ]
 then
-    echo "SOURCE_DIR is exists"
+    echo " SOURCE_DIR is exists "
 else
-    echo "SOURCE_DIR is not exists"
+    echo " SOURCE_DIR is not exists "
     exit 1
 fi
